e5702c520f012d59abbda067c968956cf7cb4244
moreen karimi <moreenk541@gmail.com>
2025-03-20 09:02:59 +0300

    DOM changes

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.style.height = "300px";
+element.style.backgroundColor = "#27647B";
+element.textContent = "You've changed what's on the screen!";
+element.style.fontSize = "24px";
+element.style.marginLeft = "30px";
+element.style.lineHeight = 2;
+element.className = "pet-listing dog";
+element.classList.remove("dog");
+element.classList.add("cat", "sale");
+main.remove()
+const newHeader =document.createElement('h1')
+newHeader.id ="victory"
+newHeader.textContent ="YOUR-NAME is the champion"
+
+
